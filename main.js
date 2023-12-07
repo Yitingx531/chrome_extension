7e2d8bbfdaa7b85cd5d3c68d9968ae5bb1920879
Hamza Hussaini <hamza@Hamzas-MacBook-Air.local>
2023-12-07 01:40:58 -0500

    added api call and html to transfer object

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ const titleField = document.querySelector("title");
 console.log(titleField);
 
 
-setTimeout(function printTitle() {
-    console.log(document.querySelector("title"))
-}, 2000);
+// setTimeout(function printTitle() {
+//     console.log(document.querySelector("title"))
+// }, 2000);
+
+
+
+
 
 
 
@@ -15,5 +19,64 @@ document.addEventListener("click", (event) => {
     
     console.log("CHANGING " , document.querySelector("title"));
     console.log(event);
-    //result.textContent = `You like ${event.target.value}`;
+
+
+    setTimeout(() => {
+        console.log("Delayed for 3 seconds.");
+      
+        let urlBefore = "https://www.omdbapi.com/?t="
+        let title = document.querySelector("title").innerText;
+        console.log("title NAME " + title);
+        title = title.replace(' - Netflix','');
+        console.log("title NAME CHANGED " + title);
+        let urlAfter = "&apikey=448456a7"
+        let fullUrl = urlBefore+title+urlAfter;
+        console.log(fullUrl);
+        //let url = 'https://www.omdbapi.com/?t=no+country+for+old+men&apikey=448456a7';
+        if (title.trim() !== "Home"){
+            callApi(fullUrl);
+        }
+
+    }, 3000);
+    // function callApi(){
+    //     fetch(fullUrl, { method: 'GET' })
+    //         .then(Result => Result.json())
+    //         .then(string => {
+        
+    //             console.log(string);
+        
+    //             console.log(`Title of our response : ` + string.Title);
+
+                
+    //         })
+    //         .catch(errorMsg => { console.log(errorMsg); });
+    //         //result.textContent = `You like ${event.target.value}`;
+    // }
   });
+  var ratingOutside  = "nothing";
+
+  function callApi(fullUrl){
+    fetch(fullUrl, { method: 'GET' })
+        .then(Result => Result.json())
+        .then(string => {
+    
+            console.log(string);
+    
+            console.log(`Title of our response : ` + string.Title);
+            
+            const td = document.getElementById("image");
+            console.log(td);
+            const image = document.createElement("img");
+            image.src = string.Poster;
+            console.log(image);
+            document.appendChild(image);
+            var yo = "YO HERE";
+            var ratingHere = string.imdbRating;
+            ratingOutside = string.imdbRating;
+        })
+        .catch(errorMsg => { console.log(errorMsg); });
+        //result.textContent = `You like ${event.target.value}`;
+}
+
+
+  
---
+++ popup.js
@@ -0,0 +1,6 @@
+console.log("IMAGE " , image)
+//document.getElementById('image').appendChild(image);
+console.log(document)
+//console.log(string);
+console.log(ratingOutside)
+document.getElementById("rating").textContent = ratingOutside;
