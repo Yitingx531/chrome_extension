f4a4355928d91d760247d3267a3555479bc72173
Hamza Hussaini <hamza@Hamzas-MacBook-Air.local>
2023-12-07 19:55:43 -0500

    final ending

--- main.js
+++ main.js
@@ -38,22 +38,8 @@ document.addEventListener("click", (event) => {
         }
 
     }, 3000);
-    // function callApi(){
-    //     fetch(fullUrl, { method: 'GET' })
-    //         .then(Result => Result.json())
-    //         .then(string => {
-        
-    //             console.log(string);
-        
-    //             console.log(`Title of our response : ` + string.Title);
-
-                
-    //         })
-    //         .catch(errorMsg => { console.log(errorMsg); });
-    //         //result.textContent = `You like ${event.target.value}`;
-    // }
+ 
   });
-  var ratingOutside  = "nothing";
 
   function callApi(fullUrl){
     fetch(fullUrl, { method: 'GET' })
@@ -70,15 +56,7 @@ document.addEventListener("click", (event) => {
     
             console.log(`Title of our response : ` + string.Title);
             
-            const td = document.getElementById("image");
-            console.log(td);
-            const image = document.createElement("img");
-            image.src = string.Poster;
-            console.log(image);
-            document.appendChild(image);
-            var yo = "YO HERE";
-            var ratingHere = string.imdbRating;
-            ratingOutside = string.imdbRating;
+        
         })
         .catch(errorMsg => { console.log(errorMsg); });
         //result.textContent = `You like ${event.target.value}`;
--- popup.js
+++ popup.js
@@ -11,15 +11,67 @@
                   "from a content script:" + sender.tab.url :
                   "from the extension");
                 console.group("GOT IT " , request)
-      if (typeof request === "object")
-      document.getElementById("rating").textContent = request.imdbRating;
-        
+      if (typeof request === "object"){
+      const ratingsArray = request.Ratings;
+      
+      document.getElementById("imdbRating").textContent = request.imdbRating;
+      const poster = document.getElementById("image");
+      console.log(poster);
+      document.getElementById('plot').textContent = request.Plot;
+      
+      //const image = document.createElement("img");
+      image.src = request.Poster;
+      console.log(image);
+      //td.appendChild(image);
         sendResponse({farewell: "goodbye"});
+        // document.getElementById('mainTable').style.borderRadius = '15rem';
+        // document.getElementById('mainTable').style.border = '2px solid blue';
+        document.getElementById('mainTable').style.width = 'fill';
+        document.getElementById('mainTable').style.height = 'fill';
+        document.getElementById('mainTable').style.backgroundColor = 'black';
+        document.getElementById('mainTable').style.border = '3px solid gold';
+        document.getElementById('mainTable').style.boxShadow = `0px -25px 20px -15px rgba(10,25,242,0.82),
+        25px 0px 20px -15px rgba(10,25,242,0.82),
+        0px 25px 20px -15px rgba(10,25,242,0.82),
+        -25px 0px 20px -15px rgba(10,25,242,0.82)`
+        document.getElementById('mainTable').style.borderRadius = '1rem';
+        // document.getElementById('top').style.font = "italic bold 20px arial,serif";
+        document.getElementById("top").setAttribute("align", "center");
+        // document.getElementById('top').style.color = '#5c00e6';
+        document.getElementById('top').style.fontWeight = '1000';
+        document.getElementById('plot').style.padding = '1.3rem';
+        document.getElementById("imdbRating").style.padding = '1rem'
+
+        // for (let i = 0; i < ratingsArray.length; i++){
+        //   console.log("RATINGS ARRAY ELEMENTS " , ratingsArray[i]);
+        //   if (i == 0){
+        //     document.getElementById("imdbRating").textContent = ratingsArray[i].Value;
+        //   }
+        //   // else if (i == 1){
+        //   //   document.getElementById("rottenRating").textContent = ratingsArray[i].Value;
+        //   // }
+        //   // else if (i == 2){
+        //   //   document.getElementById("metacriticRating").textContent = ratingsArray[i].Value;
+        //   // }
+  
+        // }
+      }
     }
   );
+
 console.log("IMAGE " , image)
 //document.getElementById('image').appendChild(image);
 console.log(document)
 //console.log(string);
 console.log(ratingOutside)
 document.getElementById("rating").textContent = ratingOutside;
+
+// table {
+//   border-radius: 25rem;
+//   border: 1px, solid blue;
+// }
+
+// #top {
+//   font-family: Arial, Helvetica, sans-serif;
+//   font-size: 2rem;
+// }
